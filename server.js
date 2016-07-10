4511913658d8e5f8cc4bd0e66f534777eef7c5f9
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-07-09 19:26:30 -0700

    working routers with async callback

--- server.js
+++ server.js
@@ -4,14 +4,9 @@ var url = require('url');
 function start(route, handle) {
   function onRequest(req,res){
     var pathname = url.parse(req.url).pathname;
-    if (pathname !== '/favicon.ico') {
-      console.log('Request for ' + pathname + ' received.');
+    console.log('Request for ' + pathname + ' received.');
 
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      var content = route(handle, pathname);
-      res.write(content);
-    }
-    res.end();
+    route(handle, pathname, res);
   }
 
   http.createServer(onRequest).listen(8888);
